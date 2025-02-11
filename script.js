4a0390692023970954c777df4b3d193f3474a0e7
Nicky <nurzhan.askarov05@gmail.com>
2025-02-11 13:30:23 +0500

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function startQuiz() {
+    let question = prompt("Қазақ тілінде қанша әріп бар?");
+    if (question == "42") {
+        alert("Дұрыс!");
+    } else {
+        alert("Қате! Дұрыс жауап: 42");
+    }
+}
